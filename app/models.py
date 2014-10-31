b1383ab69e29d9d0fd403e774fb476cbf33c3509
Rainwater <jqrvz6@mst.edu>
2014-10-31 14:11:49 -0500

    Major database infrastructure

--- app/models.py
+++ app/models.py
@@ -6,22 +6,35 @@ class Rankable(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(128))
 	type = db.Column(db.String(40))
-	rankings = db.Relationship('Ranking', backref='rankable')
+	rankings = db.relationship('Ranking', backref='rankable')
 
 	__mapper_args__ = {
 		'polymorphic_identity': 'rankable',
 		'polymorphic_on': type,
 	}
 
+student_spells = db.Table('student_spells',
+	db.Column('student_id', db.Integer, db.ForeignKey('students.id')),
+	db.Column('spell_id', db.Integer, db.ForeignKey('spells.id'))
+)
+
 class Student(Rankable):
 	__tablename__ = 'students'
 
 	id = db.Column(db.Integer, db.ForeignKey('rankables.id'), primary_key=True)
 	points = db.Column(db.Integer, default=0)
+	spells = db.relationship('Spell', secondary=student_spells, backref='casters')
+	posts = db.relationship('Post', backref='student')
 
 	__mapper_args__ = {
 		'polymorphic_identity': 'student'
 	}
+
+	def add_points(self, num):
+		self.points += num
+
+	def deduct_points(self, num):
+		self.points -= num
 
 class Spell(Rankable):
 	__tablename__ = 'spells'
@@ -53,7 +66,29 @@ class Ranking(db.Model):
 
 	rank_id = db.Column(db.Integer, db.ForeignKey('ranks.id'), primary_key=True)
 	tree_id = db.Column(db.Integer, db.ForeignKey('trees.id'), primary_key=True)
-	rank = db.Relationship('Rank', backref='rankings')
-	tree = db.Relationship('Tree', backref='rankings')
-
+	rank = db.relationship('Rank', backref='rankings')
+	tree = db.relationship('Tree', backref='rankings')
 	rankable_id = db.Column(db.Integer, db.ForeignKey('rankables.id'))
+
+	def title(self):
+		return self.rank.name + ' - ' + self.tree.name
+
+post_tags = db.Table('post_tags',
+	db.Column('post_id', db.Integer, db.ForeignKey('posts.id')),
+	db.Column('tag_id', db.Integer, db.ForeignKey('tags.id'))
+)
+
+class Post(db.Model):
+	__tablename__ = "posts"
+
+	id = db.Column(db.Integer, primary_key=True)
+	user_id = db.Column(db.Integer, db.ForeignKey('students.id'))
+	body = db.Column(db.Text)
+	created_at = db.Column(db.DateTime)
+	tags = db.relationship('Tag', secondary=post_tags, backref="posts")
+
+class Tag(db.Model):
+	__tablename__ = 'tags'
+
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(40))
---
+++ tests/test_models.py
@@ -0,0 +1,141 @@
+import unittest
+from datetime import datetime
+from app import create_app, db
+from app.models import *
+
+class ModelsTestCase(unittest.TestCase):
+	def setUp(self):
+		self.app = create_app('testing')
+		self.app_context = self.app.app_context()
+		self.app_context.push()
+		db.create_all()
+
+	def tearDown(self):
+		db.session.remove()
+		db.drop_all()
+		self.app_context.pop()
+
+	def test_default_student_points(self):
+		stud = Student()
+		db.session.add(stud)
+		db.session.commit()
+		self.assertEqual(stud.points, 0)
+
+	def test_students_have_multiple_rankings(self):
+		stud = Student(name="me")
+		rank1 = Rank(name="rank1")
+		tree1 = Tree(name="tree1")
+		ranking1 = Ranking(rankable=stud, rank=rank1, tree=tree1)
+
+		db.session.add(ranking1)
+		db.session.commit()
+
+		self.assertEqual(stud.rankings[0], ranking1)
+
+		rank2 = Rank(name="rank2")
+		tree2 = Tree(name="tree2")
+		ranking2 = Ranking(rankable=stud, rank=rank2, tree=tree2)
+
+		db.session.add(ranking2)
+		db.session.commit()
+
+		self.assertEqual(stud.rankings[1], ranking2)
+
+	def test_spells_have_multiple_rankings(self):
+		spell = Spell(name="me")
+		rank1 = Rank(name="rank1")
+		tree1 = Tree(name="tree1")
+		ranking1 = Ranking(rankable=spell, rank=rank1, tree=tree1)
+
+		db.session.add(ranking1)
+		db.session.commit()
+
+		self.assertEqual(spell.rankings[0], ranking1)
+
+		rank2 = Rank(name="rank2")
+		tree2 = Tree(name="tree2")
+		ranking2 = Ranking(rankable=spell, rank=rank2, tree=tree2)
+
+		db.session.add(ranking2)
+		db.session.commit()
+
+		self.assertEqual(spell.rankings[1], ranking2)
+
+	def test_ranking_title(self):
+		rank = Rank(name="Rank!")
+		tree = Tree(name="Tree!")
+		ranking = Ranking(rank=rank, tree=tree)
+		self.assertEqual("Rank! - Tree!", ranking.title())
+
+	def test_students_gaining_and_losing_points(self):
+		stud = Student(name="Me")
+		db.session.add(stud)
+		db.session.commit()
+
+		stud.add_points(50)
+
+		self.assertEqual(stud.points, 50)
+
+		stud.deduct_points(50)
+
+		self.assertEqual(stud.points, 0)
+
+	def test_students_have_many_spells(self):
+		stud = Student(name="Me")
+		spell1 = Spell(name="Firaga")
+		spell2 = Spell(name="Thundaga")
+		spell1.casters.append(stud)
+		spell2.casters.append(stud)
+		db.session.add(stud)
+		db.session.commit()
+
+		self.assertIn(spell1, stud.spells)
+		self.assertIn(spell2, stud.spells)
+
+	def test_spells_have_many_casters(self):
+		spell = Spell(name="Thundaga")
+		stud1 = Student(name="Lightning")
+		stud2 = Student(name="Cloud")
+		stud1.spells.append(spell)
+		stud2.spells.append(spell)
+		db.session.add(spell)
+		db.session.commit()
+
+		self.assertIn(stud1, spell.casters)
+		self.assertIn(stud2, spell.casters)
+
+	def test_spell_description(self):
+		spell = Spell(name="Thundaga", description="Big ass lightnin bolt")
+
+		self.assertEqual(spell.description, "Big ass lightnin bolt")
+
+	def test_post_created_at(self):
+		now = datetime.now()
+		post = Post(created_at=now)
+		db.session.add(post)
+		db.session.commit()
+
+		self.assertEqual(now, post.created_at)
+
+	def test_student_has_many_posts(self):
+		stud = Student(name="Someone")
+		post1 = Post(body="post 1")
+		post2 = Post(body="post 2")
+		stud.posts.append(post1)
+		stud.posts.append(post2)
+		db.session.add(stud)
+		db.session.commit()
+
+		self.assertIn(post1, stud.posts)
+		self.assertIn(post2, stud.posts)
+
+	def test_many_posts_to_many_tags(self):
+		post = Post(body="Post 1")
+		tag = Tag(name="Tag 1")
+
+		post.tags.append(tag)
+		db.session.add(post)
+		db.session.commit()
+
+		self.assertIn(tag, post.tags)
+		self.assertIn(post, tag.posts)
