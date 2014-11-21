006ffad42cc5722540a6f8deafb689f348e3a83f
Rainwater <jqrvz6@mst.edu>
2014-11-20 21:50:06 -0600

    edit profiles

--- app/main/forms.py
+++ app/main/forms.py
@@ -16,3 +16,14 @@ class SignupForm(Form):
 	def validate_name(self, field):
 		if Student.query.filter_by(name=field.data).first():
 			raise ValidationError("Name has already been taken")
+
+class ProfileForm(Form):
+	name = StringField('Username', validators=[
+        Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                          'Usernames must have only letters, '
+                                          'numbers, dots or underscores')])
+	submit = SubmitField("Submit")
+
+	def validate_name(self, field):
+		if Student.query.filter_by(name=field.data).first():
+			raise ValidationError("Name has already been taken")
--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, url_for, flash
-from flask.ext.login import login_user, login_required, logout_user
+from flask.ext.login import login_required, current_user, login_user
 from . import main
-from .forms import SignupForm, LoginForm
+from .forms import SignupForm, LoginForm, ProfileForm
 from .. import db, lm
 from ..models import Student
 
@@ -22,3 +22,19 @@ def index():
 		return redirect(url_for('.index'))
 
 	return render_template('index.html', form=form, login=login)
+
+@login_required
+@main.route('/profile/edit', methods=["GET", "POST"])
+def edit():
+	form = ProfileForm()
+
+	if form.validate_on_submit():
+		current_user.name = form.name.data
+		db.session.add(current_user)
+		db.session.commit()
+		flash("Profile updated")
+		return redirect(url_for('main.index'))
+
+	form.name.data = current_user.name
+
+	return render_template('user/edit.html', form=form)
